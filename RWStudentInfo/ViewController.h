2318d2e967735645eba370680ab9265885dd45f2
zouqianli <1004248149@qq.com>
2017-05-13 02:08:04 +0800

    Initial Commit

---
+++ RWStudentInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RWStudentInfo
+//
+//  Created by 邹前立 on 2017/5/13.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RWStudentInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RWStudentInfo
+//
+//  Created by 邹前立 on 2017/5/13.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
